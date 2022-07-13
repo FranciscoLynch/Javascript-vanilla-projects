bf5d3907ce1146153bc14251762d8a363cfd2b04
FranciscoLynch <lynch_francisco_w@hotmail.com>
2022-07-13 11:46:56 -0300

    js code commented

--- digital-clock/js/app.js
+++ digital-clock/js/app.js
@@ -1,19 +1,24 @@
+// Select the div with the id named clock
 const clock = document.querySelector('#clock');
 
+// Function to get the current hour
 const getCurrentHour = () => {
 
+    // get the complete date
     const currentDate = new Date();
+    // make an object with the hour minute and second from the currentDate
     const currentHour = {
         hour: currentDate.getHours(),
         minute: currentDate.getMinutes(),
         second: currentDate.getSeconds()
     }
 
+    // show in the div with the id named clock the value of the hour minute and second
     clock.innerHTML = `${currentHour.hour} : ${currentHour.minute} : ${currentHour.second}`;
-    console.log('updated')
 }
 
+// run the function
 getCurrentHour();
 
-
+// set the interval to 200(0,2seg) to update constantly the function and constantly update the current hour in the html
 setInterval(getCurrentHour, 200)
